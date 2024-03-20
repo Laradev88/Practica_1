5f3aa00ff61451a6bdfe03f6efe64b243f433623
Laradev88 <laradevincenti@hotmail.com>
2024-03-20 15:12:25 -0300

    En vez de numero de intentos, tiene numero de fallos

--- game.py
+++ game.py
@@ -7,7 +7,10 @@ words = ["python", "programación", "computadora", "código", "desarrollo","inte
 secret_word = random.choice(words)
 
 # Número máximo de intentos permitidos
-max_attempts = 10
+# max_attempts = 10
+
+# NUMERO MAXIMO DE ERRORES A COMETER
+errores = 0
 
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
@@ -21,8 +24,9 @@ word_displayed = "_" * len(secret_word)
 
 print(f"Palabra: {word_displayed}")
 
-for i in range(max_attempts):
+while errores < 5:
     # Pedir al jugador que ingrese una letra
+    print(f"errores: {errores}")
     letter = input("Ingresa una letra: ").lower()
 
     # Verificar si la letra ya ha sido adivinada
@@ -44,6 +48,7 @@ for i in range(max_attempts):
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
+        errores += 1
 
     # Mostrar la palabra parcialmente adivinada
     letters = []
@@ -61,5 +66,5 @@ for i in range(max_attempts):
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
 else:
-    print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+    print(f"¡Oh no! Has llegado a tus {errores} errores.")
     print(f"La palabra secreta era: {secret_word}")
